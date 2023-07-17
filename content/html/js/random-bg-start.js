c28e5c0f144834f09a6890071f4b6383f7970855
yugioh99 <139728383+yugioh99@users.noreply.github.com>
2023-07-17 01:47:53 -0400

    update background

--- content/html/js/random-bg-start.js
+++ content/html/js/random-bg-start.js
@@ -1,16 +1,30 @@
 
 function backgr(){
     var backimg = [
-        "content/img/theme/bg-laptop/01.jpg",
-        "content/img/theme/bg-laptop/02.jpg",
-        "content/img/theme/bg-laptop/03.png",
-        "content/img/theme/bg-laptop/04.jpg",
-        "content/img/theme/bg-laptop/05.jpg",
-        "content/img/theme/bg-laptop/06.png",
-        "content/img/theme/bg-laptop/07.webp",
-        "content/img/theme/bg-laptop/08.jpg"
+        "content/img/theme/bg-start/01.jpg",
+        "content/img/theme/bg-start/02.jpg",
+        "content/img/theme/bg-start/03.jpg",
+        "content/img/theme/bg-start/04.jpg",
+        "content/img/theme/bg-start/05.jpg",
+        "content/img/theme/bg-start/06.jpg",
+        "content/img/theme/bg-start/07.jpg",
+        "content/img/theme/bg-start/08.jpg",
+        "content/img/theme/bg-start/09.webp",
+        "content/img/theme/bg-start/10.webp",
+        "content/img/theme/bg-start/11.webp",
+        "content/img/theme/bg-start/12.webp",
+        "content/img/theme/bg-start/13.webp",
+        "content/img/theme/bg-start/14.webp",
+        "content/img/theme/bg-start/15.webp",
+        "content/img/theme/bg-start/16.webp",
+        "content/img/theme/bg-start/17.webp",
+        "content/img/theme/bg-start/18.png",
+
+
+
+
     ];
-    var randimg =Math.floor(Math.random()*8);
+    var randimg =Math.floor(Math.random()*18);
     document.body.background = backimg[randimg];
 }
 
